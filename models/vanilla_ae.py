76715393da549dc65123a90ad40b38f7825ee04a
ossama <ossama.ahmed@mail.mcgill.ca>
2019-03-26 08:30:04 +0100

    updated the initial skeleton

--- models/vanilla_ae.py
+++ models/vanilla_ae.py
@@ -4,20 +4,21 @@ import torch.nn as nn
 
 
 class Autoencoder(nn.Module):
+
     def __init__(self):
         super(Autoencoder, self).__init__()
-
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 6, kernel_size=5),
+            nn.Linear(28 * 28, 128),
             nn.ReLU(True),
-            nn.Conv2d(6, 16, kernel_size=5),
-            nn.ReLU(True))
+            nn.Linear(128, 64),
+            nn.ReLU(True), nn.Linear(64, 12), nn.ReLU(True), nn.Linear(12, 3))
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(16, 6, kernel_size=5),
+            nn.Linear(3, 12),
             nn.ReLU(True),
-            nn.ConvTranspose2d(6, 3, kernel_size=5),
+            nn.Linear(12, 64),
             nn.ReLU(True),
-            nn.Sigmoid())
+            nn.Linear(64, 128),
+            nn.ReLU(True), nn.Linear(128, 28 * 28), nn.Tanh())
 
     def forward(self, x):
         x = self.encoder(x)
